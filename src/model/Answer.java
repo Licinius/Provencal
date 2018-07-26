f3e0844b857c5912b90767024d06bb915171c4fa
Licinius <florent.dellomo@gmail.com>
2018-07-26 14:49:36 +0200

    Documentation : Ajout de documentation
    Ajout de documentation au constructeur et au getter et setter des
    fonctions de la classes Answer et Badge

--- src/model/Answer.java
+++ src/model/Answer.java
@@ -2,25 +2,49 @@ package model;
 
 import java.io.Serializable;
 import java.util.Date;
+
+/**
+ * An answer of a Question is also a Post
+ * @See Post
+ * @author Crauser
+ *
+ */
 @SuppressWarnings("serial")
 public class Answer extends Post implements Serializable {
-	
 	private int parentId;
-
-	//CONSTRUCTOR
+	/**
+	 * Create an answer with all the attributes (null possible)
+	 * @param id The question id
+	 * @param creationDate the date when the answer has been created
+	 * @param deletionDate the date when the answer has been deleted, null if the answer hasn't been deleted
+	 * @param score The answer score on SO
+	 * @param body The answer body 
+	 * @param lastEditDate The last time the question has been edited
+	 * @param closedDate The date when the question has been closed (null if the answer is open)
+	 * @param communityOwnedDate The date when the answer started to belong to the community
+	 * @param owner The user that owns the answer
+	 * @param lastEditor The last editor of the answer
+	 * @param parentId The parent id, i.e the question answered
+	 */
 	public Answer(int id, Date creationDate, Date deletionDate, int score, String body, Date lastEditDate,
 			Date closedDate, Date communityOwnedDate, User owner, User lastEditor, int parentId) {
 		super(id, creationDate, deletionDate, score, body, lastEditDate, closedDate, communityOwnedDate, owner, lastEditor);
 		this.parentId = parentId;
 	}
 
-	//GETTER & SETTER
+	/**
+	 * 
+	 * @return The id of the question answered
+	 */
 	public int getParentId() {
 		return parentId;
 	}
 
+	/**
+	 * 
+	 * @param parentId A id of a question 
+	 */
 	public void setParentId(int parentId) {
 		this.parentId = parentId;
 	}
-
 }
--- src/model/Badge.java
+++ src/model/Badge.java
@@ -2,6 +2,12 @@ package model;
 
 import java.io.Serializable;
 import java.util.Date;
+
+/**
+ * The badge on StackOverflow are reward for the user
+ * @author Crauser
+ *
+ */
 @SuppressWarnings("serial")
 public class Badge implements Serializable {
 	
@@ -12,7 +18,15 @@ public class Badge implements Serializable {
 	private boolean tagBased;
 	private int userId;
 	
-	//CONSTRUCTOR
+	/**
+	 * Create a badge with all the attributes (null possible)
+	 * @param id The badge id
+	 * @param name The name of the badge
+	 * @param date The date on which the user obtained the badge 
+	 * @param classBadge The class of the badge
+	 * @param tagBased The badge tag
+	 * @param userId The user id that owns the badge
+	 */
 	public Badge(int id, String name, Date date, int classBadge, boolean tagBased, int userId) {
 		super();
 		this.id = id;
@@ -23,54 +37,98 @@ public class Badge implements Serializable {
 		this.userId = userId;
 	}
 
-	//GETTER & SETTER
+	/**
+	 * 
+	 * @return An integer, the id of the badge
+	 */
 	public int getId() {
 		return id;
 	}
-
+	/**
+	 * 
+	 * @param id a integer that will be the id of badge
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	
+	/**
+	 * 
+	 * @return a string, the name of the badge (ex : Socratic,...)
+	 */
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+	 * 
+	 * @param name a new name for the badge
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	/**
+	 * 
+	 * @return the date when the user earned the badge
+	 */
 	public Date getDate() {
 		return date;
 	}
-
+	
+	/**
+	 * 
+	 * @param date A Date to sets
+	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
-
+	
+	/**
+	 * 
+	 * @return an integer representing the badge class
+	 */
 	public int getClassBadge() {
 		return classBadge;
 	}
-
+	
+	/**
+	 * 
+	 * @param classBadge A new integer for the badge class
+	 */
 	public void setClassBadge(int classBadge) {
 		this.classBadge = classBadge;
 	}
 
+	/**
+	 * 
+	 * @return true if the badge is tag based
+	 */
 	public boolean isTagBased() {
 		return tagBased;
 	}
-
+	
+	/**
+	 * 
+	 * @param tagBased set if whether or not the badge is tag based
+	 */
 	public void setTagBased(boolean tagBased) {
 		this.tagBased = tagBased;
 	}
 	
+	/**
+	 * 
+	 * @return an integer of the user id
+	 */
 	public int getUserId() {
 		return userId;
 	}
 
+	/**
+	 * 
+	 * @param userId, set a new user for the badge
+	 */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-	
-	
 }
